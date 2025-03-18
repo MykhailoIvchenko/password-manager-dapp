581fb26d61b5fdf88ee2f3b5c447715b077fb51e
Mykhailo <ivchenkomykhailo@gmail.com>
2025-03-18 22:50:08 +0200

    Add providers for error boundary, redux, toast notifications

--- src/password-manager-dapp-frontend/src/main.tsx
+++ src/password-manager-dapp-frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ErrorBoundary } from 'react-error-boundary';
+import FallBackPage from './components/FallbackPage';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import './styles/index.scss';
+import ToastProvider from './components/ToastProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ErrorBoundary fallbackRender={FallBackPage}>
+    <Provider store={store}>
+      <App />
+      <ToastProvider />
+    </Provider>
+  </ErrorBoundary>
 );
