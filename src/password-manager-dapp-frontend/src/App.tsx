f9c8815af0e5250311bfdbf215179f89520373b0
Mykhailo <ivchenkomykhailo@gmail.com>
2025-03-19 23:14:09 +0200

    Comment the wasm initialization code as it causes an error

--- src/password-manager-dapp-frontend/src/App.tsx
+++ src/password-manager-dapp-frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
 import CheckAccess from './components/CheckAccess';
 import DefaultPageLayout from './components/ui/DefaultPageLayout';
-// @ts-ignore
-import { default as vetkd_init } from './vetkd_user_lib/ic_vetkd_utils';
-// @ts-ignore
-import vetkd_wasm from './vetkd_user_lib/ic_vetkd_utils_bg';
+// // @ts-ignore
+// import { default as vetkd_init } from './vetkd_user_lib/ic_vetkd_utils.js';
+// // @ts-ignore
+// import vetkd_wasm from './vetkd_user_lib/ic_vetkd_utils_bg.wasm';
 
 function App() {
-  useEffect(() => {
-    vetkd_wasm().then((res: any) => vetkd_init(res));
-  }, []);
+  // useEffect(() => {
+  //   vetkd_wasm().then((res: any) => vetkd_init(res));
+  // }, []);
 
   return (
     <DefaultPageLayout>
